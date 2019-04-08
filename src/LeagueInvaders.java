7b86ad6b5890d4873d86b8e6d721c0a704b33039
League <league@WTS9.attlocal.net>
2019-04-07 17:30:37 -0700

    League Invaders, finished Part 4, Step 2

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,29 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
+	JFrame frame;
+	GamePanel gamePanel;
+public static void main(String[] args) {
+	LeagueInvaders l = new LeagueInvaders();
+	l.setUp();
+}
+LeagueInvaders(){
+	gamePanel = new GamePanel();
+}
+void setUp() {
+	frame = new JFrame();
+	frame.add(gamePanel);
+	frame.setVisible(true);
+	int width = 500;
+	int height = 800;
+	frame.setSize(width, height);
+	frame.getContentPane().setPreferredSize(new Dimension(width, height));
 
+    frame.pack();
+	 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	 
+	 gamePanel.startgame();
+}
 }
